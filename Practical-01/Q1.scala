09ad27b9e4d766c3a70216b9ccc76f24989a26a8
Sachith <sachithdhanushka1@gmail.com>
2024-06-17 23:37:30 +0530

    Update Q1.scala

--- Practical-01/Q1.scala
+++ Practical-01/Q1.scala
@@ -2,10 +2,10 @@ object Q1{
     val PI = 3.14
 
     // Calculate the area of a disk
-    def area (r: Double): Double = PI * r * r
+    def areaOfDisk(r: Double): Double = PI * r * r
 
 
     def main(args: Array[String]): Unit = {
-        println("Area of a disk with radius 5" + area(5))
+        println("Area of a disk with radius 5" + areaOfDisk(5))
     }
 }
