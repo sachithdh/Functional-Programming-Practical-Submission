3d54671409a87395cf405cf2d8d27808843c4f2e
sacheex <sachithdhanushka00.com>
2024-06-24 00:40:43 +0530

    Practical 02 submission

--- Practical-02/Q1.scala
+++ Practical-02/Q1.scala
@@ -6,14 +6,14 @@ object Q1{
         var f, g: Float = 12.0f
         var c: Char = 'X'
 
-        println(s"k + 12 * m => ${k + 12 * m}")
-        println(s"m / j => ${m / n}")
-        println(s"n % j => ${n%j}")
-        println(s"m / j * j => ${m / j * j}")
-        println(s"f + 10*5 + g => ${f + 10 * 5 + g}")
+        println(s"a). ${k + 12 * m}")
+        println(s"b). ${m / n}")
+        println(s"c). ${n%j}")
+        println(s"d). ${m / j * j}")
+        println(s"e). ${f + 10 * 5 + g}")
 
         i += 1
-        println(s"++i * n => ${i * n}")
+        println(s"f). ${i * n}")
     }
 }
 
--- Practical-02/Q2.scala
+++ Practical-02/Q2.scala
@@ -8,24 +8,24 @@ object Q2{
         
         // - -b * a + c *d - -
         b -= 1
-        println(s"--b * a + c * d-- => ${b* a + c *d}") // Ommit d -1 because what d-- do is just decrease the val of d
+        println(s"a). ${b* a + c *d}") // Ommit d -1 because what d-- do is just decrease the val of d
         d -= 1
 
         // a++
-        println(s"a++ => $a")
+        println(s"b). $a")
         a += 1
 
         // –2 * ( g – k ) +c
-        // println(s"-2 * (g - k)+c => ${-2 * (g - k)+c}")
+        // println(s"c). ${-2 * (g - k)+c}")
 
         // c=c++
-        println(s"$c")
+        println(s"d). $c")
         c += 1
 
         // c=++c*a++
         c += 1
         c = c * a
-        println(s"${c}")
+        println(s"e). ${c}")
         a += 1
 
         
